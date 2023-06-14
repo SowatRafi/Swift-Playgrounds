021b2622f65636319a56bbb21156187f4a857567
SowatRafi <sowad.rafi@icloud.com>
2023-06-14 12:03:17 +0600

    Constants, variables, and data-types in Swift-5

--- Swift Programming/Constants, variables, data types.playground/Contents.swift
+++ Swift Programming/Constants, variables, data types.playground/Contents.swift
@@ -1,3 +1,24 @@
-import Cocoa
+import Foundation
 
-var greeting = "Hello, playground"
+/*
+ Constant (let) - A value that cannot be changed
+ Variable (var) - A value that can be changed
+ 
+ */
+
+let name: String = "Sowad"
+var age: Int = 25
+
+/// cannot use another --> var age: Int = 25
+
+print(name, age)
+
+/*
+ Type-safe language
+ 
+ Mixing of different data types and values is restricted
+ */
+
+let day: String = "Wednesday"
+let dailyTemperature = 33
+print("\nThe temperature on \(day) is \(dailyTemperature)°C.")
